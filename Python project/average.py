1a66fdc8999f9d7cf4c3716fc9e72af6c0e16c6f
daayaan3288 <dayannn@gmail.com>
2023-09-19 22:58:34 +0330

    Bankaccount

--- Python project/average.py
+++
@@ -1,5 +0,0 @@
-
-numbers = [1, 2, 3, 4, 5]
-average = sum(numbers) / len(numbers)
-
-print("میانگین اعداد در لیست:", average)
---
+++ average
@@ -0,0 +1,5 @@
+
+numbers = [1, 2, 3, 4, 5]
+average = sum(numbers) / len(numbers)
+
+print("میانگین اعداد در لیست:", average)
